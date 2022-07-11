e5d4990fbb4484633ebf41ce5260c34c5328192b
ashutoshkumar00 <akashutosh82@gmail.com>
2022-07-11 21:01:00 +0530

    Updates

---
+++ precache-manifest.d45e62c32c8ee15cf5a76b75e750e891.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0ef3df577cfff5283480",
+    "url": "/TextUtils-React%20App/static/css/main.4c9631f7.chunk.css"
+  },
+  {
+    "revision": "0ef3df577cfff5283480",
+    "url": "/TextUtils-React%20App/static/js/main.0ef3df57.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/TextUtils-React%20App/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "5b977f20bbc5700a25d3",
+    "url": "/TextUtils-React%20App/static/js/2.5b977f20.chunk.js"
+  },
+  {
+    "revision": "c310de1e16d8678b6c60",
+    "url": "/TextUtils-React%20App/static/js/runtime~main.c310de1e.js"
+  },
+  {
+    "revision": "1ea5dc41308dac0b2664e0221c184524",
+    "url": "/TextUtils-React%20App/index.html"
+  }
+];
